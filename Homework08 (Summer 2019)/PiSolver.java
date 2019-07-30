48c22ca0634a053c636bfd34214071cb6308a1bf
SuperSneakySpyGuy <44660208+SuperSneakySpyGuy@users.noreply.github.com>
2019-07-30 15:32:52 -0700

    Minor Cleanup

--- Homework08 (Summer 2019)/PiSolver.java
+++ Homework08 (Summer 2019)/PiSolver.java
@@ -38,24 +38,24 @@ public class PiSolver {
         double centerX = r;
         double centerY = r;
         double estimatedPi = 0;
-        int modLength = 1000000; // Size of Time Slice
-        long startTime = System.currentTimeMillis(); //Records what time the program starts for later calculation
+        int modLength = 1000000;                                                                   // Size of Time Slice
+        long startTime = System.currentTimeMillis();                                               //Records what time the program starts for later calculation
         //System.out.println("Start Time: " + startTime);
         System.out.println("Calculating...");
         for (long i = 0; i <= points; i++){
-            if (i % modLength == 1 && i != 1){ //The numbers would be too small if we calculated how long each iteration takes, so we are timing how long modlength iterations takes (in this case 1000000) and using that as one "Timing loop"
-                long currentTime = System.currentTimeMillis(); // Pulls current time in the loop
-                long timeElapsed = currentTime - startTime; // Determines how much time has passed between starting the process and now
-                double loopTime = timeElapsed / (i/modLength); // Takes how much time is passed and divides it by what number time loop we are on (A time loop is what im calling the group of 1000000 iterations defined by our modLength). This gives us how long it takes to complete one time loop
-                double totalTime = loopTime * (points/modLength); // Multiplies the time it takes to complete one time loop by the number of time loops needed to complete the process. (points/modLength gives us the number of time loops in the whole process). Gives us how long the program is predicted to take
-                double timeRemaining = totalTime - timeElapsed; // Subtracts the total time the process is predicted to take by how much time has passed. 
-                System.out.print("\r"); // This is here so the line below keeps replacing itself instead of creating new lines every update. 
-                System.out.print("Time Remaining: " + df.format(timeRemaining / 1000) + " || "); // Time remaining will count down by seconds
+            if (i % modLength == 1 && i != 1){                                                     //The numbers would be too small if we calculated how long each iteration takes, so we are timing how long modlength iterations takes (in this case 1000000) and using that as one "Timing loop"
+                long currentTime = System.currentTimeMillis();                                     // Pulls current time in the loop
+                long timeElapsed = currentTime - startTime;                                        // Determines how much time has passed between starting the process and now
+                double loopTime = timeElapsed / (i/modLength);                                     // Takes how much time is passed and divides it by what number time loop we are on (A time loop is what im calling the group of 1000000 iterations defined by our modLength). This gives us how long it takes to complete one time loop
+                double totalTime = loopTime * (points/modLength);                                  // Multiplies the time it takes to complete one time loop by the number of time loops needed to complete the process. (points/modLength gives us the number of time loops in the whole process). Gives us how long the program is predicted to take
+                double timeRemaining = totalTime - timeElapsed;                                    // Subtracts the total time the process is predicted to take by how much time has passed. 
+                System.out.print("\r");                                                            // This is here so the line below keeps replacing itself instead of creating new lines every update. 
+                System.out.print("Time Remaining: " + df.format(timeRemaining / 1000) + " || ");   // Time remaining will count down by seconds
                 System.out.print("Estimated Pi: " + df2.format(estimatedPi));
             }
-            double x = (Math.floor(Math.random() * (r*2)) + 1); // Random x co-ordinate
+            double x = (Math.floor(Math.random() * (r*2)) + 1);                                    // Random x co-ordinate
             double y = (Math.floor(Math.random() * (r*2)) + 1); // Random y co-ordinate
-            double d = Math.sqrt((Math.pow(centerX - x, 2)) + (Math.pow(centerY - y, 2))); // Determines distance from center
+            double d = Math.sqrt((Math.pow(centerX - x, 2)) + (Math.pow(centerY - y, 2)));         // Determines distance from center
             if(debugOn == true){
                 System.out.println("X: " + x);
                 System.out.println("Y: " + y);
