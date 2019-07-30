b73d567d6705b5681c193bf934e1c0c5a0ec292d
SuperSneakySpyGuy <44660208+SuperSneakySpyGuy@users.noreply.github.com>
2019-07-30 10:24:55 -0700

    Updated Pi Solver with "time remaining" feature

--- Homework08 (Summer 2019)/PiSolver.java
+++ Homework08 (Summer 2019)/PiSolver.java
@@ -29,7 +29,25 @@ public class PiSolver {
         double centerX = r;
         double centerY = r;
         double estimatedPi = 0;
-        for (int i = 0; i <= points; i++){
+        long startTime = System.currentTimeMillis();
+        System.out.println("Start Time: " + startTime);
+        System.out.println("Calculating...");
+        for (long i = 1; i < points; i++){
+            if (i % 10000000 == 1 && i != 1){
+
+                long currentTime = System.currentTimeMillis();
+                // System.out.println("Current Time: " + currentTime);
+                long timeElapsed = currentTime - startTime;
+                // System.out.println("Time Elapsed = " + timeElapsed);
+                double loopTime = timeElapsed / (i/10000000);
+                // System.out.println("Loop Time: " + timeElapsed + " / " + i + " = " + loopTime);
+                double totalTime = loopTime * (points/10000000);
+                // System.out.println("Total Estimated Time = " + totalTime);
+                double timeRemaining = totalTime - timeElapsed;
+                System.out.print("\r");
+                System.out.print("Time Remaining: " + (timeRemaining / 1000) + " seconds");
+                // System.out.println("----------------------------------------------------------");
+            }
             double x = (Math.floor(Math.random() * (r*2)) + 1);
             double y = (Math.floor(Math.random() * (r*2)) + 1);
             double d = Math.sqrt((Math.pow(centerX - x, 2)) + (Math.pow(centerY - y, 2)));
@@ -52,8 +70,10 @@ public class PiSolver {
                 System.out.println("Total Points: " + total);
                 System.out.println("Estimated Pi: " + estimatedPi);
                 System.out.println("-------------------------------------------");
-            }
+            } 
         }
+        System.out.println("\n------------- Finished! -------------");
+        System.out.println("Finished in:  " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds.");
         return estimatedPi;
     }
 
